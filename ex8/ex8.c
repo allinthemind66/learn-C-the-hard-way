f93e464a4fe3096a1cf72cd1cc3216331b524ecb
allinthemind66 <r.nilon92@gmail.com>
2018-10-04 14:56:53 -0400

    finish exercise 8

---
+++ ex8/ex8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char*argv[])
+{
+	
+	if(argc == 2){
+		printf("You only have one argument that you passed in.\n");
+	} else if(argc < 2){
+
+		printf("The only argument passed in is ./ex8. \n");
+	}
+	 else if (argc > 2 && argc < 5){
+		printf("Here's your arguments\n");
+		
+		for(int i = 1; i < argc; i++){
+			printf("%s ", argv[i]);
+		}
+		printf("\n");
+	} else {
+		printf("You have too many arguments.\n");
+	}
+	return 0;
+}
